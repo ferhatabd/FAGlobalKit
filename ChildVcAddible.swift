86e99f8076d6fd1294bca7fd0abc84e8351312e0
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2019-09-28 19:16:12 +0300

    sturcture updates

---
+++ ChildVcAddible.swift
@@ -0,0 +1,8 @@
+//
+//  ChildVcAddible.swift
+//  FAStoryKit
+//
+//  Created by Ferhat Abdullahoglu on 27.09.2019.
+//
+
+import Foundation
---
+++ ChildVcPannible.swift
@@ -0,0 +1,8 @@
+//
+//  ChildVcPannible.swift
+//  FAGlobalKit
+//
+//  Created by Ferhat Abdullahoglu on 27.09.2019.
+//
+
+import Foundation
